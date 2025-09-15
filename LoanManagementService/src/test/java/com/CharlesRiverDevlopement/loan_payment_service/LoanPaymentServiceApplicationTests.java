353e64dfb2a4f9905673c89a097eac8f7a7bf61b
Meet <19bce071@nirmauni.ac.in>
2025-09-16 01:50:05 +0530

    Fixing the test run github action build.

--- LoanManagementService/src/test/java/com/CharlesRiverDevlopement/loan_payment_service/LoanPaymentServiceApplicationTests.java
+++ LoanManagementService/src/test/java/com/CharlesRiverDevlopement/loan_payment_service/LoanPaymentServiceApplicationTests.java
@@ -2,10 +2,30 @@ package com.CharlesRiverDevlopement.loan_payment_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class LoanPaymentServiceApplicationTests {
 
+
+  @Container
+  static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:15")
+          .withDatabaseName("testdb")
+          .withUsername("user")
+          .withPassword("password");
+
+  @DynamicPropertySource
+  static void registerPgProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+    registry.add("spring.datasource.username", postgresContainer::getUsername);
+    registry.add("spring.datasource.password", postgresContainer::getPassword);
+  }
   @Test
   void contextLoads() {}
 }
